2f28243d8d012c00cc67dc35693f4b215af8a354
chouette11 <keifukuda1115@gmail.com>
2023-05-20 19:32:18 +0900

    Add user entity

---
+++ lib/entity/user_entity.dart
@@ -0,0 +1,15 @@
+import 'dart:typed_data';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_entity.freezed.dart';
+
+@freezed
+class UserEntity with _$UserEntity {
+  const UserEntity._();
+
+  const factory UserEntity({
+    required String name,
+    required Uint8List imageValue,
+  }) = _UserEntity;
+}
