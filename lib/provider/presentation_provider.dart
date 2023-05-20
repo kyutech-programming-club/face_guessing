9a91e180d891e7b2c88c09cfb0f7a97881bd9c92
yu371 <yuyu14191567@gmail.com>
2023-05-21 00:30:55 +0900

    add user entity provider

--- lib/provider/presentation_provider.dart
+++ lib/provider/presentation_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:camera/camera.dart';
+import 'package:face_guessing/entity/user_entity.dart';
 import 'package:face_guessing/provider/domain_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -21,3 +22,28 @@ final cameraProvider = FutureProvider.autoDispose<CameraController?>(
 );
 
 final faceImageProvider = StateProvider<Uint8List?>((ref) => null);
+
+class UserEntitiesNotifier extends Notifier<List<UserEntity>> {
+    // We initialize the list of userEntities to an empty list
+    @override
+    List<UserEntity> build() {
+        return [];
+    }
+
+    void addUserEntity(UserEntity userEntity) {
+        state = [...state, userEntity];
+    }
+
+    void removeUserEntity(String userEntityName) {
+        state = [
+            for (final userEntity in state)
+                if (userEntity.name != userEntityName) userEntity,
+        ];
+    }
+}
+
+// Finally, we are using NotifierProvider to allow the UI to interact with
+// our userEntitiesNotifier class.
+final userEntitiesProvider = NotifierProvider<UserEntitiesNotifier, List<UserEntity>>(() {
+    return UserEntitiesNotifier();
+});
