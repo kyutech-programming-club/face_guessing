259cbee24daefc98af9c8e9641cf8c118e639057
kawadatakeki <kawada.takeki424@mail.kyutech.jp>
2023-05-21 14:20:23 +0900

    fixed mask widget speed and move

--- lib/page/quiz/component/face.dart
+++ lib/page/quiz/component/face.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:face_guessing/page/quiz/component/mask_widget.dart';
@@ -16,6 +17,7 @@ class Face extends StatefulWidget {
 class _FaceState extends State<Face> {
 
   // カウンター
+  double counter = 0;
   double top_counter = 0;
   double left_counter = 0;
 
@@ -26,11 +28,13 @@ class _FaceState extends State<Face> {
     // 1秒ごとに _counterを1ずつ足していく
     Timer.periodic(
       // 第一引数：繰り返す間隔の時間を設定
-      const Duration(seconds: 1),
+      const Duration(milliseconds: 100),
       // 第二引数：その間隔ごとに動作させたい処理を書く
           (Timer timer) {
-        top_counter++;
-        left_counter++;
+        int R = 70;
+        counter = counter + 0.001;
+        top_counter = 3*R*sin(counter) - 2*R*sin(3/2*R*counter);
+        left_counter = 3*R*cos(counter) - 2*R*cos(3/2*R*counter);
         setState(() {});
       },
     );
@@ -54,11 +58,11 @@ class _FaceState extends State<Face> {
           child: Center(
             child: Container(
               color: Colors.white,
-              height: 100,
-              width: 100,
+              height: 30,
+              width: 30,
+              ),
             ),
           )
-      ),
     );
   }
 }
